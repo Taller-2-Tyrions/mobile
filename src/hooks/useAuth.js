51b17301d38a6f4faef852f15676ab28a7f2def1
gsabatino <gsabatino@fi.uba.ar>
2022-10-17 21:05:58 -0300

    mejoro la recepción de las push notifications para que ande siempre

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,22 +1,28 @@
-import React, { useEffect, useState, useMemo, createContext, useContext } from 'react';
-import * as Google from 'expo-auth-session/providers/google';
-import * as WebBrowser from 'expo-web-browser';
-import axios from 'axios';
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  createContext,
+  useContext,
+} from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import axios from "axios";
 import {
-    GoogleAuthProvider,
-    onAuthStateChanged,
-    signInWithCredential,
-    getAuth
-} from 'firebase/auth';
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  getAuth,
+} from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBSenFicB4rNCqRO183gmoMILDImbTR84Y",
-    authDomain: "fiuber-36b86.firebaseapp.com",
-    projectId: "fiuber-36b86",
-    storageBucket: "fiuber-36b86.appspot.com",
-    messagingSenderId: "388259755156",
-    appId: "1:388259755156:web:04d82df1a410135ee9f081"
+  apiKey: "AIzaSyBSenFicB4rNCqRO183gmoMILDImbTR84Y",
+  authDomain: "fiuber-36b86.firebaseapp.com",
+  projectId: "fiuber-36b86",
+  storageBucket: "fiuber-36b86.appspot.com",
+  messagingSenderId: "388259755156",
+  appId: "1:388259755156:web:04d82df1a410135ee9f081",
 };
 
 initializeApp(firebaseConfig);
@@ -25,105 +31,108 @@ const AuthContext = createContext({});
 WebBrowser.maybeCompleteAuthSession();
 
 export function AuthProvider({ children }) {
-    const [user, setUser] = useState({
-        accessToken: null,
-        id: null,
-        formComplete: null,        
-    });
+  const [user, setUser] = useState({
+    accessToken: null,
+    id: null,
+    formComplete: null,
+  });
 
-    const signIn = async (data) => {
-        const url = 'https://fiuber-gateway.herokuapp.com/login';
-        
-        axios
-            .post(url, {
-                "email": data.email,
-                "password": data.password
-            })
-            .then((res) => {
-                const {token, is_registered, id} = res.data;
-    
-                if (!token) {
-                    console.log('No token error');
-                } else {                    
-                    const userAux = {
-                        accessToken: token,
-                        id: id,
-                        formComplete: is_registered
-                    }
+  const signIn = async (data) => {
+    const url = "https://fiuber-gateway.herokuapp.com/login";
 
-                    setUser(userAux);
-                }
-            })
-            .catch(err => {
-                console.log('signIn error: ', err);
-            });
-    };
+    axios
+      .post(url, {
+        email: data.email,
+        password: data.password,
+      })
+      .then((res) => {
+        const { token, is_registered, id } = res.data;
 
-    const register = async (data) => {        
-        const url = 'https://fiuber-gateway.herokuapp.com/signup';
-        
-        await axios
-            .post(url, {
-                "email": data.email,
-                "password": data.password
-            })
-            .catch(err => {
-                console.warn(err.detail.message);
-            });
+        if (!token) {
+          console.log("No token error");
+        } else {
+          const userAux = {
+            accessToken: token,
+            id: id,
+            formComplete: is_registered,
+          };
 
-        await signIn(data);
-    };
+          setUser(userAux);
+        }
+      })
+      .catch((err) => {
+        console.log("signIn error: ", err);
+      });
+  };
 
-    const completeForm = async (accessToken, data) => {
-        const url = 'https://fiuber-gateway.herokuapp.com/users';
+  const register = async (data) => {
+    const url = "https://fiuber-gateway.herokuapp.com/signup";
 
-        axios
-            .post(url,
-            {
-                "name": data.name,
-                "last_name": data.lastname,
-                "roles": ["Passenger"],
-                "address": data.defaultAddress,
-            }, 
-            {
-                headers: {
-                    token: accessToken,
-                },
-            })
-            /*.then((res) => {
+    await axios
+      .post(url, {
+        email: data.email,
+        password: data.password,
+      })
+      .catch((err) => {
+        console.warn(err.detail.message);
+      });
+
+    await signIn(data);
+  };
+
+  const completeForm = async (accessToken, data) => {
+    const url = "https://fiuber-gateway.herokuapp.com/users";
+
+    axios.post(
+      url,
+      {
+        name: data.name,
+        last_name: data.lastname,
+        roles: ["Passenger"],
+        address: data.defaultAddress,
+      },
+      {
+        headers: {
+          token: accessToken,
+        },
+      }
+    );
+    /*.then((res) => {
                 console.log('El usuario terminó el form');
             })
             .catch(err => {
                 console.log(err);
             })*/
-            setUser({
-                ...user,
-                formComplete: true,
-            });
-    };
+    setUser({
+      ...user,
+      formComplete: true,
+    });
+  };
 
-    const logout = () => {
-        setUser({
-            accessToken: null,
-            id: null,
-            formComplete: null,
-        });
-    };
+  const logout = () => {
+    setUser({
+      accessToken: null,
+      id: null,
+      formComplete: null,
+    });
+  };
 
+  const memoedValue = useMemo(
+    () => ({
+      user,
+      signIn,
+      register,
+      logout,
+      completeForm,
+    }),
+    [user, signIn, register, logout, completeForm]
+  );
 
-    const memoedValue = useMemo(() => ({
-       user, signIn, register, logout, completeForm
-    }), [
-        user, signIn, register, logout, completeForm
-    ]);
-
-    return (
-        <AuthContext.Provider value={memoedValue}>
-            {children}
-        </AuthContext.Provider>
-    );
-};
+  return (
+    <AuthContext.Provider value={memoedValue}>{children}</AuthContext.Provider>
+  );
+}
 
 export default function useAuth() {
-    return useContext(AuthContext);
-};
+  return useContext(AuthContext);
+}
---
+++ src/hooks/usePushNotification.js
@@ -0,0 +1,113 @@
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  createContext,
+  useContext,
+  useCallback,
+  useRef,
+} from "react";
+import * as Notifications from "expo-notifications";
+import { useNavigation } from "@react-navigation/native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+const PushNotificationContext = createContext({});
+
+export function PushNotificationProvider({ children }) {
+  const [expoToken, setExpoToken] = useState(null);
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  const navigation = useNavigation();
+
+  const requestPermission = async () => {
+    try {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        throw new Error("Permission not granted!");
+      }
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      //const token = (await Notifications.getDevicePushTokenAsync()).data;
+      setExpoToken(token);
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!expoToken) {
+      requestPermission();
+    }
+
+    console.log("Expo token: ", expoToken);
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        const {
+          notification: {
+            request: {
+              content: {
+                data: { screen },
+              },
+            },
+          },
+        } = response;
+        if (screen) {
+          navigation.navigate(screen);
+        }
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, [expoToken]);
+
+  const memoedValue = useMemo(
+    () => ({
+      requestPermission,
+      expoToken,
+    }),
+    [requestPermission, expoToken]
+  );
+
+  return (
+    <PushNotificationContext.Provider value={memoedValue}>
+      {children}
+    </PushNotificationContext.Provider>
+  );
+}
+
+export default function usePushNotification() {
+  return useContext(PushNotificationContext);
+}
